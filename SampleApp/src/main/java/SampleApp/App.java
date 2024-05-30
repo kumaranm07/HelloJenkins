aba7cc9afaf9220c59af9c8287e742fecb7ce6da
kumaran07 <balakumaranm2002@gmail.com>
2024-05-31 02:35:29 +0530

    First Commit

---
+++ SampleApp/src/main/java/SampleApp/App.java
@@ -0,0 +1,13 @@
+package SampleApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Jenkins Pipeline!" );
+    }
+}
